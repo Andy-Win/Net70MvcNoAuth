01d348f39a1e39d76ec99f3826e478b5e932491c
Andy Wang <v-wajian@microsoft.com>
2023-04-10 00:04:16 -0700

    1

---
+++ Net70MvcNoAuth/Migrations/20230410070159_blogs.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Net70MvcNoAuth.Migrations
+{
+    /// <inheritdoc />
+    public partial class blogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
--- Net70MvcNoAuth/Program.cs
+++ Net70MvcNoAuth/Program.cs
@@ -1,11 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Net70MvcNoAuth
 {
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        { }
+        public DbSet<Blog> Blogs { get; set; } = null!;
+    }
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string? Url { get; set; }
+    }
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            var connectionString = builder.Configuration.GetConnectionString("BlogConnection") ?? throw new InvalidOperationException();
+            builder.Services.AddDbContext<BloggingContext>(options => options.UseSqlServer(connectionString));
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
